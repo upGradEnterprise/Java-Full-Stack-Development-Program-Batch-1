c642acd7426fd33740de8420b924d48e2476c710
Akash <akash300383@gmail.com>
2025-10-20 13:05:08 +0530

    done

---
+++ Programs/Core Java Programs/JavaOOPsConcept/src/finalkeyword/FinalKeywordExample.java
@@ -0,0 +1,23 @@
+package finalkeyword;
+final class A {
+    final void display1() {
+        System.out.println("A class method");
+    }
+}
+//class B extends A {
+////    @Override
+////    void display1() {
+////        System.out.println("B class override display1 method");
+////    }
+//}
+public class FinalKeywordExample {
+    public static void main(String[] args) {
+        final int A=100;
+        System.out.println(A);
+        //A=200;
+//        B obj1 = new B();
+//        obj1.display1();
+        A obj2 = new A();
+        obj2.display1();
+    }
+}
