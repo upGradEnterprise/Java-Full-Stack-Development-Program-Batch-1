edfcf770b0472c5e0e0df412c6730008e2da3549
Akash <akash300383@gmail.com>
2025-10-29 14:59:26 +0530

    done

--- Programs/Core Java Programs/Java910And11FeaturesTest/src/immutablecollections/ImmutableCollectionsExamples.java
+++ Programs/Core Java Programs/Java910And11FeaturesTest/src/immutablecollections/ImmutableCollectionsExamples.java
@@ -10,7 +10,7 @@ public class ImmutableCollectionsExamples {
         names.add("C");
         //names = Collections.unmodifiableList(names);
         names.add("D");
-        // factory method provided to achieve immutable collection 
+        // factory method provided to achieve immutable collection
         List ll1 = List.of("A","B","C");
         Set ss1 = Set.of("A","B","C");
         Map mm1 = Map.of("key1","value1","key2","value2");
---
+++ Programs/Core Java Programs/JavaNewFeatures/src/sealedexamples/JavaSealedAndPermitsExample.java
@@ -0,0 +1,37 @@
+package sealedexamples;
+sealed class Payment permits CreditCard,UpiPayment{
+    public void payment() {
+        System.out.println("Generic Payment Logic");
+    }
+}
+sealed class CreditCard  extends  Payment {
+    @Override
+    public void payment() {
+        System.out.println("Credit Card Payment");
+    }
+}
+final class MasterCreditCard extends CreditCard {
+    @Override
+    public void payment() {
+        System.out.println("Credit Card Payment through master card");
+    }
+}
+final class UpiPayment extends  Payment {
+    @Override
+    public void payment() {
+        System.out.println("Upi Payment");
+    }
+}
+//class PhonePay extends Payment {
+//
+//}
+public class JavaSealedAndPermitsExample {
+    public static void main(String[] args) {
+    Payment payment1 = new CreditCard();
+    payment1.payment();
+    Payment payment2 = new MasterCreditCard();
+    payment2.payment();
+    Payment payment3 = new UpiPayment();
+    payment3.payment();
+    }
+}
