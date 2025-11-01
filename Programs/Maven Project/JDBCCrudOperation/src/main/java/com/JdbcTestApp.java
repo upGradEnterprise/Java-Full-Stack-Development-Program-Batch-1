0d2dd38470c75ba2282b77c70b7f820319f4363c
Akash <akash300383@gmail.com>
2025-11-01 16:01:46 +0530

    done

--- Programs/Maven Project/JDBCCrudOperation/src/main/java/com/JdbcTestApp.java
+++ Programs/Maven Project/JDBCCrudOperation/src/main/java/com/JdbcTestApp.java
@@ -1,14 +1,16 @@
 package com;
 import java.sql.*;
+import java.util.Scanner;
+
 public class JdbcTestApp {
     public static void main(String[] args) {
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver Loaded...");
             Connection con =
-                    DriverManager.getConnection("jdbc:mysql://localhost:3306/upgrade_db","root","root@123");
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/upgrade_db", "root", "root@123");
             System.out.println("Connected successfully");
-            Statement stmt = con.createStatement();
+            //Statement stmt = con.createStatement();
             // create table product(pid int primary key,pname varchar(30), price float);
             // insert query
 
@@ -36,15 +38,85 @@ public class JdbcTestApp {
 //            }
 
             // retrieve query
-            ResultSet rs  = stmt.executeQuery("select * from Product");
-            while(rs.next()){
-                System.out.println("PId "+rs.getInt(1)+" PName is "+rs.getString(2)+" Price is "+rs.getFloat(3));
+//            ResultSet rs  = stmt.executeQuery("select * from Product");
+//            while(rs.next()){
+//                System.out.println("PId "+rs.getInt(1)+" PName is "+rs.getString(2)+" Price is "+rs.getFloat(3));
+//            }
+
+            //rs.close();
+            Scanner sc = new Scanner(System.in);
+
+//            // insert the recod using PreparedStatement
+//            PreparedStatement pstmt = con.prepareStatement("insert into product values(?,?,?)");
+//            System.out.println("Enter the id");
+//            int pid = sc.nextInt();
+//            pstmt.setInt(1,pid);
+//
+//            System.out.println("Enter the pname");
+//            String pname = sc.next();
+//            pstmt.setString(2,pname);
+//
+//            System.out.println("Enter the price");
+//            float price = sc.nextFloat();
+//            pstmt.setFloat(3,price);
+//
+//
+//            int result = pstmt.executeUpdate();
+//            if(result>0){
+//                System.out.println("Record inserted successfully");
+//            }
+
+
+            // delete the recod using PreparedStatement
+//            PreparedStatement pstmt = con.prepareStatement("delete from product where pid =?");
+//            System.out.println("Enter the id");
+//            int pid = sc.nextInt();
+//            pstmt.setInt(1,pid);
+//            int result = pstmt.executeUpdate();
+//            if(result>0){
+//                System.out.println("Record deleted successfully");
+//            }else {
+//                System.out.println("Record not present");
+//            }
+
+
+            // update the recod using PreparedStatement
+//            PreparedStatement pstmt = con.prepareStatement("update product set price = ? where pid = ?");
+//            System.out.println("Enter the id");
+//            int pid = sc.nextInt();
+//            pstmt.setInt(2,pid);
+//
+//            System.out.println("Enter the price");
+//            float price = sc.nextFloat();
+//            pstmt.setFloat(1,price);
+//
+//
+//            int result = pstmt.executeUpdate();
+//            if(result>0){
+//                System.out.println("Record updated successfully");
+//            }else {
+//                System.out.println("Record not present");
+//            }
+
+            // retrieve record using pid
+            PreparedStatement pstmt = con.prepareStatement("select * from product where pid = ?");
+            System.out.println("Enter the id");
+            int pid = sc.nextInt();
+            pstmt.setInt(1,pid);
+
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                System.out.println("Pid is "+rs.getInt("pid")+" PName is "+rs.getString(2)+" Price is "+rs.getFloat(3));
+            }else {
+                System.out.println("record not present");
             }
-            rs.close();
-            stmt.close();
+
+            //stmt.close();
             con.close();
         }   catch (Exception e){
             System.out.println(e);
         }
+
+
     }
 }
