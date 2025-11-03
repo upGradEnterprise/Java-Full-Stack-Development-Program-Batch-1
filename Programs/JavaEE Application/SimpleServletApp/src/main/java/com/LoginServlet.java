d2887548051a6967117849b7ec9bea5a75f3db2e
Akash <akash300383@gmail.com>
2025-11-03 15:00:16 +0530

    done

--- Programs/JavaEE Application/SimpleServletApp/src/main/java/com/LoginServlet.java
+++ Programs/JavaEE Application/SimpleServletApp/src/main/java/com/LoginServlet.java
@@ -1,5 +1,6 @@
 package com;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,12 +14,34 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter pw = resp.getWriter();
+        RequestDispatcher rd1 = req.getRequestDispatcher("index.html"); // html page
+        RequestDispatcher rd2 = req.getRequestDispatcher("Home");       //servlet page
         String emailId = req.getParameter("emailId");
         String password = req.getParameter("password");
         if(emailId.equals("admin@gmail.com") && password.equals("admin@123")){
-            pw.println("<html><body><h1>Successfully login by Admin</h1></body></html>");
+            pw.println("<html><body><h1>Successfully login by Admin with Get method</h1></body></html>");
+            rd2.forward(req, resp);
         }else {
-            pw.println("<html><body><h1>Failure try once again</h1></body></html>");
+            pw.println("<html><body><h1>Failure try once again with Get method</h1></body></html>");
+            rd1.include(req, resp);
         }
+        resp.setContentType("text/html");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+        String emailId = req.getParameter("emailId");
+        String password = req.getParameter("password");
+        RequestDispatcher rd1 = req.getRequestDispatcher("index.html"); // html page
+        RequestDispatcher rd2 = req.getRequestDispatcher("Home");       //servlet page
+
+        if(emailId.equals("admin@gmail.com") && password.equals("admin@123")){
+            pw.println("<html><body><h1>Successfully login by Admin with Post method</h1></body></html>");
+            rd2.forward(req, resp);
+        }else {
+            pw.println("<html><body><h1>Failure try once again with Post method</h1></body></html>");
+            rd1.include(req, resp);
+        }
+        resp.setContentType("text/html");
     }
 }
