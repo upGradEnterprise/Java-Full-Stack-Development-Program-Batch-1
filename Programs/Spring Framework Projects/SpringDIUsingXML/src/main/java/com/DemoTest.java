fc40c9ec7b72c77cb3b51ac7729474099c6277a9
Akash <akash300383@gmail.com>
2025-11-04 15:36:28 +0530

    done

--- Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/DemoTest.java
+++ Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/DemoTest.java
@@ -12,17 +12,26 @@ public class DemoTest {
 
         Resource res  = new ClassPathResource("beans.xml"); // load the beans.xml file from resource directory
         BeanFactory factory = new XmlBeanFactory(res);  // created BeanFactory interface reference.
+//
+//        Employee e1 = (Employee) factory.getBean("emp1");
+//        e1.display();
+//
+//        Employee e2 = (Employee) factory.getBean("emp1");
+//        e2.display();
+//        System.out.println("---------------------------");
+//        Employee e3 = (Employee) factory.getBean("emp2");
+//        e3.display();
+//
+//        Employee e4 = (Employee) factory.getBean("emp2");
+//        e4.display();
 
-        Employee e1 = (Employee) factory.getBean("emp1");
-        e1.display();
+        Employee e5 = (Employee) factory.getBean("emp1");
+        System.out.println(e5);
 
-        Employee e2 = (Employee) factory.getBean("emp1");
-        e2.display();
-        System.out.println("---------------------------");
-        Employee e3 = (Employee) factory.getBean("emp2");
-        e3.display();
+        Employee e6 = (Employee) factory.getBean("emp2");
+        System.out.println(e6);
 
-        Employee e4 = (Employee) factory.getBean("emp2");
-        e4.display();
+        Employee e7 = (Employee) factory.getBean("emp3");
+        System.out.println(e7);
     }
 }
--- Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/Employee.java
+++ Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/Employee.java
@@ -1,10 +1,31 @@
 package com;
 
 public class Employee {
+    private int id;
+    private String name;
+    private float salary;
+
     public void display() {
         System.out.println("display method");
     }
     Employee() {
-        System.out.println("object created");
+        System.out.println("object created with empty constructor");
+
+    }
+
+    public Employee(int id, String name, float salary) {
+        System.out.println("object created with parameterized constructor");
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
