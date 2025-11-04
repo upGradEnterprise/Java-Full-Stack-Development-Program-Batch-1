d69846981f52fe243724689b8c5c996ea8799589
Akash <akash300383@gmail.com>
2025-11-04 16:12:03 +0530

    done

--- Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/DemoTest.java
+++ Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/DemoTest.java
@@ -25,16 +25,22 @@ public class DemoTest {
 //        Employee e4 = (Employee) factory.getBean("emp2");
 //        e4.display();
 
-        Employee e5 = (Employee) factory.getBean("emp1");
-        System.out.println(e5);
+//        Employee e5 = (Employee) factory.getBean("emp1");
+//        System.out.println(e5);
+//
+//        Employee e6 = (Employee) factory.getBean("emp2");
+//        System.out.println(e6);
+//
+//        Employee e7 = (Employee) factory.getBean("emp3");
+//        System.out.println(e7);
+//
+//        Employee e8 = (Employee) factory.getBean("emp4");
+//        System.out.println(e8);
 
-        Employee e6 = (Employee) factory.getBean("emp2");
-        System.out.println(e6);
-
-        Employee e7 = (Employee) factory.getBean("emp3");
-        System.out.println(e7);
-
-        Employee e8 = (Employee) factory.getBean("emp4");
-        System.out.println(e8);
+        Address a1 = (Address)factory.getBean("add1");
+        System.out.println(a1);
+        System.out.println("--------------------");
+        Employee e9 = (Employee) factory.getBean("emp5");
+        System.out.println(e9);
     }
 }
--- Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/Employee.java
+++ Programs/Spring Framework Projects/SpringDIUsingXML/src/main/java/com/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
     private int id;
     private String name;
     private float salary;
+    private Address add;
 
     public void display() {
         System.out.println("display method");
@@ -20,13 +21,29 @@ public class Employee {
         this.salary = salary;
     }
 
+    public Employee(int id, String name, float salary, Address add) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.add = add;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", salary=" + salary +
+                ", add=" + add +
                 '}';
+    }
+
+    public Address getAdd() {
+        return add;
+    }
+
+    public void setAdd(Address add) {
+        this.add = add;
     }
 
     public int getId() {
