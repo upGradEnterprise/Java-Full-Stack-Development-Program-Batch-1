c97ac0fa204d7cb51bc1511fb5403eeb4b9f1718
Akash <akash300383@gmail.com>
2025-11-06 12:58:03 +0530

    done

---
+++ Programs/Spring boot Projects/SpringBootDemoTest/SpringBootDemoTest/src/main/java/com/SpringBootDemoTestApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoTestApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootDemoTestApplication.class, args);
+        System.out.println("Spring boot up");
+	}
+
+}
