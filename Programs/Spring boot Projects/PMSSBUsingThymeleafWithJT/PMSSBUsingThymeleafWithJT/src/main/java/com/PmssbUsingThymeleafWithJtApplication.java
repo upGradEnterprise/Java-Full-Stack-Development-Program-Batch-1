7380cd1467b6015533b09a137e91e3e17090265a
Akash <akash300383@gmail.com>
2025-11-06 14:33:16 +0530

    done

---
+++ Programs/Spring boot Projects/PMSSBUsingThymeleafWithJT/PMSSBUsingThymeleafWithJT/src/main/java/com/PmssbUsingThymeleafWithJtApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmssbUsingThymeleafWithJtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmssbUsingThymeleafWithJtApplication.class, args);
+	}
+
+}
