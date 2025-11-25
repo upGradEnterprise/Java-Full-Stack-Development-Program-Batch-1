bb10d1b55cc788c6bfdc634fc3c6bad4b704ec8e
Akash <akash300383@gmail.com>
2025-11-25 12:52:56 +0530

    done

--- UI Technology/JavaScript using ES6/typesOfFunctions.js
+++ UI Technology/JavaScript using ES6/typesOfFunctions.js
@@ -1,23 +1,37 @@
-// normal function style 
-// function no passing parameter and no return type 
-function greet() {
-    document.write("Hello User!<br>");
-}
-greet(); // function call
+// // normal function style 
+// // function no passing parameter and no return type 
+// function greet() {
+//     document.write("Hello User!<br>");
+// }
+// greet(); // function call
 
-// function with passing parameter and no return type
-function add(a,b){
-    var sum = a+b;
-    document.write("<br/>Sum is: " + sum + "<br>");
-}
-add(5,10); // function call with arguments
-add(100)
-add()
-add("A","B")
+// // function with passing parameter and no return type
+// function add(a,b){
+//     var sum = a+b;
+//     document.write("<br/>Sum is: " + sum + "<br>");
+// }
+// add(5,10); // function call with arguments
+// add(100)
+// add()
+// add("A","B")
 
-// function with pasing parameter and return type
-function checkLogin(emailId,password){
-    return emailId=="admin@gmail.com" && password=="admin123"?"successfully login":false;
+// // function with pasing parameter and return type
+// function checkLogin(emailId,password){
+//     return emailId=="admin@gmail.com" && password=="admin123"?"successfully login":false;
+// }
+// document.writeln("<br/>" + checkLogin("admin@gmail.com","admin123"));
+// document.writeln("<br/>" + checkLogin("admin@gamil.com","admin1234"));
+
+// Normal function 
+display1(); // JavaScript hosting concept
+function display1() {
+    document.write("<br/>Normal Function Called<br/>");
 }
-document.writeln("<br/>" + checkLogin("admin@gmail.com","admin123"));
-document.writeln("<br/>" + checkLogin("admin@gamil.com","admin1234"));
+display1();
+
+// Expression style function 
+//display3();// Error: display3 is not a function, it does't support hosting
+var display3 = function() {
+    document.write("<br/>Expression Function Called<br/>");
+}
+display3();
