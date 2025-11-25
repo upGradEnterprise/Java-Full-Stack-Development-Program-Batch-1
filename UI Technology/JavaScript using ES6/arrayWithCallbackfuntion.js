a9cd7d25b6ea6599093462c52f58c2b975995e60
Akash <akash300383@gmail.com>
2025-11-25 15:56:43 +0530

    done

---
+++ UI Technology/JavaScript using ES6/arrayWithCallbackfuntion.js
@@ -0,0 +1,14 @@
+let num = [1,2,3,4,5,6,7,8,9,10];
+document.write("Array callback function example:<br>");
+// add element in between as well remove element from array 
+// 1st parameter index position
+// 2nd parameter number of elements to be removed
+// 3rd, 4th, ....to add the elements
+document.write("Original array: " + num + "<br>");
+
+//num.splice(3,1);        // remove 3 index position element 
+//num.splice(3,4);        // from 4th index position remove 4 elements
+num.splice(3,0,100);    // at 3rd index position add 100 without removing any element
+//num.splice(3,0,100,200,300,400);    // at 3rd index position add 100 without removing any element
+num.splice(3,4,100,200,300,400);    // at 3rd index position add replace with old value as 100,200,300,400
+document.write("<br/>Modified array: " + num + "<br>");
