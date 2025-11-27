b86dc483832a509ac94b379e8c854e41b4f84dc8
Akash <akash300383@gmail.com>
2025-11-27 15:11:26 +0530

    done

--- UI Technology/TypeScript Programs/functionType.js
+++ UI Technology/TypeScript Programs/functionType.js
@@ -3,7 +3,15 @@
 //     console.log("simple typescript function")
 // }
 // greeting();
-
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // // function with number of parameter as well their data types as any but number of parameter must be match 
 // function add(a:any,b:any){
 //     console.log(a+b)
@@ -18,7 +26,6 @@
 // addNumber(100,200)
 // addNumber(100.10,200.20);
 // //addNumber("A","B")
-
 // // function with no return type ie any type consider. 
 // // function dis1() {
 // //     //return 100;
@@ -27,18 +34,14 @@
 // // }  
 // // this function can return any value as well as no return it by default consider 
 // function dis1() : any {
-
 // }
 // // no return type then write void. 
 // function dis2() : void {
-    
 // }
 // // function with return type ie string ,number, boolean, etc 
 // function sayHello(name:string):string {
 //     return "Welcome to TypeScript "+name
 // }
-
-
 // // function with optional parameter 
 // function empInfo(id?:number,name?:string,age?:number): void {
 //     console.log("id is "+id+" name is "+name+" age is "+age)
@@ -47,7 +50,6 @@
 // empInfo(101,"Ramesh")
 // empInfo(102)
 // empInfo();
-
 // function with default initialization 
 // function empInfo(id:number=0,name:string="Unknown",age:number=0.0): void {
 //     console.log("id is "+id+" name is "+name+" age is "+age)
@@ -57,28 +59,28 @@
 // empInfo(102)
 // empInfo();
 // function with array parameter 
-
 // function empInfo(id:number,name:string,age:number,skillSet:string[]):void {
 //     console.log("id is "+id+" name is "+name+" Age is "+age+" skillSet "+skillSet)
 // }
 // empInfo(1,"Ravi",21,[]);
 // empInfo(2,"Ramesh",24,["Java","Python"]);
 // empInfo(3,"Raju",26,["HTML"])
-
-
 // function with rest operator 
-function empInfo(id:number,name:string,age:number,...skillSet:string[]):void {
-    console.log("id is "+id+" name is "+name+" Age is "+age)
-    console.log("Number of skillSet "+skillSet.length)
-    for(let skill of skillSet){
-        console.log(skill)
+function empInfo(id, name, age) {
+    var skillSet = [];
+    for (var _i = 3; _i < arguments.length; _i++) {
+        skillSet[_i - 3] = arguments[_i];
+    }
+    console.log("id is " + id + " name is " + name + " Age is " + age);
+    console.log("Number of skillSet " + skillSet.length);
+    for (var _a = 0, skillSet_1 = skillSet; _a < skillSet_1.length; _a++) {
+        var skill = skillSet_1[_a];
+        console.log(skill);
     }
 }
-let skillInfo:string[]=["HTML","CSS","JavaScript","TypeScript","Angular"];
-
-empInfo(1,"Ravi",21);   // 0 parameter 
-empInfo(2,"Ramesh",23,"C")  // 1 
-empInfo(3,"Lokesh",24,"Java","Python","React JS")  // many 
-empInfo(4,"Ajay",28,skillInfo[0],skillInfo[1],skillInfo[2],skillInfo[3],skillInfo[4]);
-empInfo(5,"Mahesh",29,...skillInfo);    // using spread we can array value individually 
-
+var skillInfo = ["HTML", "CSS", "JavaScript", "TypeScript", "Angular"];
+empInfo(1, "Ravi", 21); // 0 parameter 
+empInfo(2, "Ramesh", 23, "C"); // 1 
+empInfo(3, "Lokesh", 24, "Java", "Python", "React JS"); // many 
+empInfo(4, "Ajay", 28, skillInfo[0], skillInfo[1], skillInfo[2], skillInfo[3], skillInfo[4]);
+empInfo.apply(void 0, __spreadArray([5, "Mahesh", 29], skillInfo, false)); // using spread we can array value individually 
