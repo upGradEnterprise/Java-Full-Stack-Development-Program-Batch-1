afa448fec3fb05b7c0a3a034888e15016c7d91cc
Akash <akash300383@gmail.com>
2025-11-27 10:18:24 +0530

    done

---
+++ UI Technology/Synchronous and Asynchronous programs/asynchronous.js
@@ -0,0 +1,10 @@
+//console.log("1st statement")
+setTimeout(()=> {
+    console.log("1st statement")
+},3000)
+setTimeout(()=> {
+    console.log("2nd statement")
+},2000)
+setInterval(()=> {
+    console.log("3rd statement")
+},500)
---
+++ UI Technology/Synchronous and Asynchronous programs/synchronous.js
@@ -0,0 +1,3 @@
+console.log("1st statement")
+console.log("2nd statement")
+console.log("3rd statement")
