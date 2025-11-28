05c09a511579699eb266561b4128d0f7ef108abb
Akash <akash300383@gmail.com>
2025-11-28 15:04:58 +0530

    done

---
+++ UI Technology/Sample-typescript-app/target/index.js
@@ -0,0 +1,5 @@
+import { add, sub } from "./operation.js";
+console.log("it is simple ts file");
+console.log("sum of two number is " + add(100, 200));
+console.log("sub of two number is " + sub(100, 200));
+//# sourceMappingURL=index.js.map
---
+++ UI Technology/Sample-typescript-app/target/operation.d.ts
@@ -0,0 +1,3 @@
+export declare function add(a: number, b: number): number;
+export declare function sub(a: number, b: number): number;
+//# sourceMappingURL=operation.d.ts.map
