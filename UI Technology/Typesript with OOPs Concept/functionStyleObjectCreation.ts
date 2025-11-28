a4ae57075324ae587bf8845cc517c85d7fee9711
Akash <akash300383@gmail.com>
2025-11-28 13:10:50 +0530

    done

--- UI Technology/Typesript with OOPs Concept/functionStyleObjectCreation.ts
+++ UI Technology/Typesript with OOPs Concept/functionStyleObjectCreation.ts
@@ -1,13 +1,28 @@
-function Employee() {
+// function Employee() {
+//     this.id = 100;
+//     this.name = "Ravi";
+//     this.age = 21;
+//     this.displayEmployeeInfo = function() {
+//         console.log("id is "+this.id)
+//         console.log("name is "+this.name)
+//         console.log("age is "+this.age)
+//     }
+// }
+
+// let emp1 = new Employee();
+// emp1.displayEmployeeInfo();
+
+interface EmployeeType {
+        id:number;
+        name:string;
+        age?:number;
+}
+function Employee(this:EmployeeType){
     this.id = 100;
     this.name = "Ravi";
+    //this.salary = 34000;
     this.age = 21;
-    this.displayEmployeeInfo = function() {
-        console.log("id is "+this.id)
-        console.log("name is "+this.name)
-        console.log("age is "+this.age)
-    }
 }
-
-let emp1 = new Employee();
-emp1.displayEmployeeInfo();
+//let emp1 = new Employee();        // Error 
+let emp1 = new (Employee as any);   // object creation of function in typescript in strict mode 
+console.log(emp1.id+" "+emp1.name+" "+emp1.age)     
