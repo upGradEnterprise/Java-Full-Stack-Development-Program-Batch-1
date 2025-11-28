18039058e17f648549155d399ca3bb1240e41b5b
Akash <akash300383@gmail.com>
2025-11-28 12:46:19 +0530

    done

--- UI Technology/Typesript with OOPs Concept/classStyleObjectCreation.ts
+++ UI Technology/Typesript with OOPs Concept/classStyleObjectCreation.ts
@@ -22,20 +22,59 @@
 
 
 // typescript with constructor short cut initialization 
-class Customer {
-    constructor(private id:number,private name:string="Unknown",public age:number=18){ // automatically variable consider as instance if we use private or public 
+// class Customer {
+//     constructor(private id:number,private name:string="Unknown",public age:number=18){ // automatically variable consider as instance if we use private or public 
    
+//     }
+//     displayEmpInfo():void {
+//         console.log("id is "+this.id)
+//         console.log("name is "+this.name)
+//         console.log("age is "+this.age)
+//     }
+// }
+// let cust1 = new Customer(100,"Ravi",21)
+// let cust2 = new Customer(101,"Raju")
+// let cust3 = new Customer(103)
+// cust1.displayEmpInfo();
+// cust2.displayEmpInfo();
+// cust3.displayEmpInfo();
+// console.log(cust1.age)
+
+// Simple inheritance example 
+
+// class Employee {
+//     displayEmp(): void {
+//         console.log("Employee class function")
+//     }
+// }
+// class Manager extends Employee {
+//     displayMgr(): void {
+//         console.log("Manager class function")
+//     }
+// }
+// let emp1 = new Employee();
+// let mgr1 = new Manager();
+// emp1.displayEmp();
+// mgr1.displayEmp();
+// mgr1.displayMgr();
+
+// interface with only function 
+interface Bank {
+    withdrawn(): void;
+    deposit(): void;
+}
+class Hdfc implements Bank {
+    withdrawn(): void {
+        
     }
-    displayEmpInfo():void {
-        console.log("id is "+this.id)
-        console.log("name is "+this.name)
-        console.log("age is "+this.age)
+    deposit(): void {
+        
     }
 }
-let cust1 = new Customer(100,"Ravi",21)
-let cust2 = new Customer(101,"Raju")
-let cust3 = new Customer(103)
-cust1.displayEmpInfo();
-cust2.displayEmpInfo();
-cust3.displayEmpInfo();
-console.log(cust1.age)
+
+
+
+
+
+
+
---
+++ UI Technology/Typesript with OOPs Concept/literalStyleObjectCreation.ts
@@ -0,0 +1,7 @@
+interface EmployeeType {
+    id:number;
+    name:string;
+    age?:number;
+}
+let emp1:EmployeeType = {id:100,name:"Ravi",age:21}
+let emp2:EmployeeType = {id:103,name:"Raju"}
