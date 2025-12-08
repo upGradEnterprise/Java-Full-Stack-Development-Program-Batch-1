2e2d51218b9afb34bb4e4a68a7aef05efb24a7e6
Akash <akash300383@gmail.com>
2025-12-08 11:20:39 +0530

    done

--- UI Technology/React JS/my-react-app/src/EmployeeComponent.tsx
+++ UI Technology/React JS/my-react-app/src/EmployeeComponent.tsx
@@ -1,9 +1,16 @@
 function EmployeeComponent() {
-
+let id:number = 100;
+let fname:string = "Raj Deep";
+let isActive:boolean = false;        // local variable. 
+let skillSet:Array<string>=["Java","Python","ReactJS","HTML","CSS"]
     return(
         <>
             <h3>Employee Component</h3>
-            <p>Employee id is </p>
+            <p>Employee id is {id} name is {fname} and status {isActive?"Active ":"InActive"}</p>
+            <p>SkillSet {skillSet.join(" ")}</p>
+            <ul>
+                {skillSet.map(s=><li>{s}</li>)}
+            </ul>
         </>
     )
 }
