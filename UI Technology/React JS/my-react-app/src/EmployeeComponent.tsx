7e4abd54e5336833073627ae3b3472917a61e47d
Akash <akash300383@gmail.com>
2025-12-08 10:59:24 +0530

    done

---
+++ UI Technology/React JS/my-react-app/src/EmployeeComponent.tsx
@@ -0,0 +1,11 @@
+function EmployeeComponent() {
+
+    return(
+        <>
+            <h3>Employee Component</h3>
+            <p>Employee id is </p>
+        </>
+    )
+}
+
+export default EmployeeComponent
