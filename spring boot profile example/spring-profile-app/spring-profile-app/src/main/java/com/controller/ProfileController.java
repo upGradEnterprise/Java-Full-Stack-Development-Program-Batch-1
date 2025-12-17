4d115ef76372d6d1ec2f9cdf0ca50c8527f31fa2
Akash <akash300383@gmail.com>
2025-12-17 17:36:58 +0530

    done

--- spring boot profile example/spring-profile-app/spring-profile-app/src/main/java/com/controller/ProfileController.java
+++ spring boot profile example/spring-profile-app/spring-profile-app/src/main/java/com/controller/ProfileController.java
@@ -1,4 +1,17 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProfileController {
+
+    @Value("${app.message}")
+    private String value;
+
+    @GetMapping(value = "profile")
+    public String getProfile() {
+        return "Your active profile is "+value;
+    }
 }
